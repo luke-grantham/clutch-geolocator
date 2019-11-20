76a7c140d31f3d4ba2694d9ada4b02c1562e8739
luke-grantham <Luke@Lukes-MBP.hsd1.ga.comcast.net>
2019-11-19 20:15:54 -0500

    Handle bad results from API and bad user input cases
    Handle case where Google API finds no results
    Handle case where Google API sends some unknown error
    Handle case where user does not send the correct header

--- geolocator-server/server.py
+++ geolocator-server/server.py
@@ -18,10 +18,21 @@ def geocode(addr):
  
     response = requests.get(url)
 
-    return (response.json()['results'][0]['geometry']['location']['lat'], response.json()['results'][0]['geometry']['location']['lng'])
+    if response.json()['status'] == 'OK':
+        return (response.json()['results'][0]['geometry']['location']['lat'], response.json()['results'][0]['geometry']['location']['lng'])
+    elif response.json()['status'] == 'ZERO_RESULTS':
+        return (1000,"Zero Results from Google API")
+    else:
+        return (1001,"Unknown Error from Google Maps API")
 
 def get_state_from_coords(lat_lng):
-    """take a 2-tuple of floats representing latitude and longitude. return the containing US state"""
+    """take a 2-tuple of floats representing latitude and longitude. return the containing US state or an error message"""
+
+    # Check for errors from Google Maps API
+    if lat_lng[0] == 1000:
+        return lat_lng[1]
+    elif lat_lng[0] == 1001:
+        return lat_lng[1]
 
     conn = psycopg2.connect(host=docker_gateway_ip, port = 5432, database="postgres", user="postgres")
     cur = conn.cursor()
@@ -40,7 +51,10 @@ def get_state_from_coords(lat_lng):
 
 @app.route('/address',methods = ['GET'])
 def index():
-    return get_state_from_coords(geocode(request.headers['address']))
+    try:
+        return get_state_from_coords(geocode(request.headers['address']))
+    except KeyError as e:
+        return "Your request must include a header named 'address'"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
